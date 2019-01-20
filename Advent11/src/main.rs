137c18234a99ca1ca0229f82ec9590d31947b4c5
kalleakerblom <kalleakerblom@gmail.com>
2019-01-20 01:07:47 +0100

    Refactor A11 to use a struct for integral image

--- Advent11/src/main.rs
+++ Advent11/src/main.rs
@@ -9,51 +9,66 @@ fn calc_grid_val(x: i32, y: i32, serial: i32) -> i32 {
     power -= 5;
     power
 }
-fn calc_integral_image(serial: i32) -> [[i32; 300]; 300] {
-    let mut ii = [[0; 300]; 300];
-    for y in 0..300 {
-        let mut row_sum = 0;
-        for x in 0..300 {
-            let current_cell = calc_grid_val(1 + x as i32, 1 + y as i32, serial);
-            row_sum += current_cell;
-            let above_sum = if y == 0 { 0 } else { ii[y - 1][x] };
-            ii[y][x] = row_sum + above_sum;
-        }
-    }
-    ii
-}
-fn sum_from_integral_image(ii: &[[i32; 300]; 300], upper_left: (usize, usize), size: usize) -> i32 {
-    let (x, y) = upper_left;
-    if size == 0 {
-        return 0;
-    }
-    let mut sum = ii[y + size - 1][x + size - 1];
-    if x > 0 {
-        sum -= ii[y + size - 1][x - 1];
-    }
-    if y > 0 {
-        sum -= ii[y - 1][x + size - 1];
-    }
-    if x > 0 && y > 0 {
-        sum += ii[y - 1][x - 1];
-    }
-    sum
-}
+
 fn main() {
     let serial = 6548;
-    let integral_img = calc_integral_image(serial);
+    let ii = IntegralImage::new(
+        300,
+        300,
+        Box::new(move |x, y| calc_grid_val(1 + x as i32, 1 + y as i32, serial)),
+    );
     let mut max = (i32::min_value(), (0, 0, 0));
     for x in 0..300 {
         for y in 0..300 {
             //determine size range
             let max_size = 300 - cmp::max(x, y);
             for size in 1..max_size {
-                let sum = sum_from_integral_image(&integral_img, (x, y), size);
+                let sum = ii.get_rectangle_sum((x, y), size).expect("get sum");
                 if sum > max.0 {
                     max = (sum, (x + 1, y + 1, size));
                 }
             }
         }
     }
-    println!("ans2: {:?}", max.1);
+    println!("ans2v2: {:?}", max.1);
+}
+
+struct IntegralImage {
+    sums: Vec<Vec<i32>>,
+}
+
+impl IntegralImage {
+    fn new(width: usize, length: usize, generator: Box<Fn(usize, usize) -> i32>) -> IntegralImage {
+        let mut sums = vec![vec![0; width]; length];
+        for y in 0..length {
+            let mut row_sum = 0;
+            for x in 0..width {
+                let current_cell = generator(x, y);
+                row_sum += current_cell;
+                let above_sum = if y == 0 { 0 } else { sums[y - 1][x] };
+                sums[y][x] = row_sum + above_sum;
+            }
+        }
+
+        IntegralImage { sums }
+    }
+
+    /// Will return None if rectangle size is larger than bounds of the integral image
+    fn get_rectangle_sum(&self, upper_left: (usize, usize), size: usize) -> Option<i32> {
+        let (x, y) = upper_left;
+        if size == 0 {
+            return Some(0);
+        }
+        let mut sum = *self.sums.get(y + size - 1)?.get(x + size - 1)?;
+        if x > 0 {
+            sum -= self.sums.get(y + size - 1)?.get(x - 1)?;
+        }
+        if y > 0 {
+            sum -= self.sums.get(y - 1)?.get(x + size - 1)?;
+        }
+        if x > 0 && y > 0 {
+            sum += self.sums.get(y - 1)?.get(x - 1)?;
+        }
+        Some(sum)
+    }
 }
